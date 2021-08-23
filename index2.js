f30b45d09e4cf202dee132587d704fff85005350
Anh Tong <86377459+kimanhtong@users.noreply.github.com>
2021-08-23 14:27:55 -0400

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,1 +1,14 @@
+const { nextISSTimesForMyLocation } = require (`./iss_promised`);
 
+const printPassTimes = (passTimes) => {
+  passTimes.forEach(passTime => {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(passTime.risetime);
+    console.log(`Next pass at ${datetime} for ${passTime.duration} seconds!`);
+  });
+};
+nextISSTimesForMyLocation ()
+.then ((response) => {printPassTimes(response)})
+.catch((error) => {
+  console.log("It didn't work: ", error.message);
+});
